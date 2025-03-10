038a882486349715d87b2c2b06fa722f206dc797
tharun <tharunbaikani8055@gmail.com>
2025-03-10 15:57:11 +0530

    changed Frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000/api";
+const API_URL = process.env.REACT_APP_API_URL || "/api";
 
 //  Fetch current weather 
 export const fetchWeather = async (location) => {
